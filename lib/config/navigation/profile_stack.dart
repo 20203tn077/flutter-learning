00dcfc438e5c3f9b17f8377f0ad9798a8c8a7826
Rebecca C. <20203tn077@utez.edu.mx>
2023-10-26 15:48:23 -0600

    asd

---
+++ lib/config/navigation/profile_stack.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfileStack extends StatelessWidget {
+  const ProfileStack({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+      }
+  );
+}
